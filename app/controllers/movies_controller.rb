db99d59613ce009018c0bf5173c0791a51b06d6d
Harris Mir <harris_mir@hotmail.com>
2012-10-26 19:30:09 -0700

    Upto

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,5 @@
 class MoviesController < ApplicationController
-
+  @all_ratings = ['G']
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
